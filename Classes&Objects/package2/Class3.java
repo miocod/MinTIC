a7a0e0af16a880a39b89798e0590ca8c883cd0bf
pancadiv <137123411+pancadiv@users.noreply.github.com>
2023-07-07 19:42:27 -0500

    Add files via upload

---
+++ Classes&Objects/package2/Class3.java
@@ -0,0 +1,11 @@
+package TJAVA.POO.package2;
+
+import TJAVA.POO.package1.Class1;
+
+public class Class3 {
+    public static void main(String[] args) {
+        Class1 object1 = new Class1();
+        object1.attribute = 15;
+        
+    }
+}
