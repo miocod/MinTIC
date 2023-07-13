f7b58b70ff95964b5053411ca5ca11989a522f84
pancadiv <137123411+pancadiv@users.noreply.github.com>
2023-07-12 20:10:50 -0500

    Add files via upload

---
+++ Classes&Objects/Package2B/Class1.java
@@ -0,0 +1,23 @@
+//Getter and Setter
+package TJAVA.POO.Package2B;
+
+public class Class1{
+    private int years;
+    private String name;
+    public void setName(String name) {//set name
+        this.name = name;
+    }
+    public String getName() {//return name
+        return name;
+    }
+    
+    public void setOld(int years){ 
+        this.years=years;
+
+    }
+    public int getYears(){
+        return years;
+    }
+    
+
+}
---
+++ Classes&Objects/Package2B/Class2.java
@@ -0,0 +1,14 @@
+//Getter and Setter
+package TJAVA.POO.Package2B;
+
+public class Class2{
+    public static void main(String[] args) {
+        Class1 object1=new Class1();
+        object1.setOld(20);
+        System.out.println("La edad es: "+object1.getYears());
+        object1.setName("Andy");
+        System.out.println("El nombre es: "+object1.getName());
+    }
+
+    
+}
