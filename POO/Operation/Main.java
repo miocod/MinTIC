519f47f99dca7850a1c52e6d228bd9641476b3a2
pancadiv <137123411+pancadiv@users.noreply.github.com>
2023-09-03 13:27:32 -0500

    Add files via upload

---
+++ POO/Operation/Main.java
@@ -0,0 +1,13 @@
+package TJAVA.POO.Operation;
+
+public class Main {
+    public static void main(String[] args) {
+        Operation op=new Operation();
+        op.readNumbers();
+        op.add();
+        op.substract();
+        op.multiply();
+        op.divide();
+        op.print();
+    }
+}
